a8909d3bad4736e0d8a2358f1bdc4e7a66b902ef
IOgnen <ogneniloski@gmail.com>
2023-10-20 18:32:58 +0200

    create project

---
+++ test/java/com/CustomerCare/CustomerCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CustomerCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
