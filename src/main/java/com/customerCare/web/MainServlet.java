de99f37f065bb7c3e1dc01e8d8a1579323afda38
vlatkodonchev <donchevvlatko@gmail.com>
2023-11-10 15:12:24 +0100

    HttpServlet

--- src/main/java/com/customerCare/web/MainServlet.java
+++ src/main/java/com/customerCare/web/MainServlet.java
@@ -1,32 +1,15 @@
 package com.customerCare.web;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.thymeleaf.context.IContext;
-import org.thymeleaf.spring6.SpringTemplateEngine;
-import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import java.io.IOException;
-
-@CrossOrigin(origins = "*")
-@WebServlet
+@Controller
 public class MainServlet extends HttpServlet {
 
-    private final SpringTemplateEngine springTemplateEngine;
-
-    public MainServlet(SpringTemplateEngine springTemplateEngine) {
-        this.springTemplateEngine = springTemplateEngine;
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        this.springTemplateEngine.process("index.html", (IContext) getServletContext());
-
+    @GetMapping
+    public String doGet() {
+        return "index";
     }
 
 }
